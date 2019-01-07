1429597fbe361c9882d2cfb5956039851c0a2ddc
Olivia Considerit <olivia.denbu@gmail.com>
2019-01-07 16:11:25 +0100

    - Added StartPage.cs and added a heading property - Created SiteGroupNames.cs for managing the attribute COntentType (GruopName) with best practice

---
+++ AlloyTraining/Models/Pages/StartPage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+
+namespace AlloyTraining.Models.Pages
+{
+    [ContentType(DisplayName = "Start",
+                 GUID = "6e202f38-481c-488c-90c5-d79092f83b40",
+                 GroupName = SiteGroupNames.Specialized,
+                 Order = 10,
+                 Description = "The home page for a website with an area blocks and partial pages.")]
+    public class StartPage : PageData
+    {
+        [CultureSpecific]
+        [Display(
+            Name = "Heading",
+            Description = "If the page heading is not set, it will fallback to Name",
+            GroupName = SystemTabNames.Content,
+            Order = 10)]
+        public virtual string Heading { get; set; }
+
+        [CultureSpecific]
+        [Display(
+            Name = "Main body",
+            Description = "The main body will be shown in the main content area of the page, using the XHTML-editor you can insert for example text, images and tables.",
+            GroupName = SystemTabNames.Content,
+            Order = 20)]
+        public virtual XhtmlString MainBody { get; set; }
+    }
+}
