62666e8e72a6068a575a79fe3feb41ea3de17a74
Olivia Considerit <olivia.denbu@gmail.com>
2019-01-08 15:24:08 +0100

    - Added SvgFile.cs - Added a property Thumbnail for handeling binarydata

---
+++ AlloyTraining/Models/Media/SvgFile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.Framework.Blobs;
+using EPiServer.Framework.DataAnnotations;
+
+namespace AlloyTraining.Models.Media
+{
+    [ContentType(DisplayName = "SVG File",
+                 GUID = "840e52a9-67c5-44e9-b88b-9ee3d1fc4a46",
+                 Description = "Use this to upload Scalable Vector Graphic (SVG) images.")]
+    [MediaDescriptor(ExtensionString = "svg")]
+    public class SvgFile : ImageData
+    {
+        public override Blob Thumbnail { get => base.BinaryData; }
+    }
+}
