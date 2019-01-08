10c72a37452123b9dbb5b94061eb39a60638ae40
Olivia Considerit <olivia.denbu@gmail.com>
2019-01-08 14:58:11 +0100

    - Added AnyFile.cs in the media folder

---
+++ AlloyTraining/Models/Media/AnyFile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+
+namespace AlloyTraining.Models.Media
+{
+    [ContentType(DisplayName = "Any File",
+                 GUID = "a970210e-0805-4d68-8e42-80a0a9223c37",
+                 Description = "Use this to upload any type of file")]
+    public class AnyFile : MediaData
+    {
+        
+    }
+}
