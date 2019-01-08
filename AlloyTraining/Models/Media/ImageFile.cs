a575a6b09c5a5cc940b669506a8d29ed6b095428
Olivia Considerit <olivia.denbu@gmail.com>
2019-01-08 15:18:09 +0100

    - Added ImageFile.cs to be able to upload images in the correct way

---
+++ AlloyTraining/Models/Media/ImageFile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.Framework.DataAnnotations;
+
+namespace AlloyTraining.Models.Media
+{
+    [ContentType(DisplayName = "ImageFile",
+                 GUID = "c5bb1392-059e-4796-85b9-c5e4de95c14e",
+                 Description = "Use this to upload image files")]
+    [MediaDescriptor(ExtensionString = "png,gif,jpg,jpeg")]
+    public class ImageFile : ImageData
+    {
+
+        [CultureSpecific]
+        [Editable(true)]
+        public virtual string Description { get; set; }
+
+    }
+}
