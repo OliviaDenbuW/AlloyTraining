a673a9d961215bc7baf342752af81938c5ae6593
Olivia Considerit <olivia.denbu@gmail.com>
2019-01-09 13:13:42 +0100

    - Added Logout() in PageControllerBase<T>.cs wich enables every page to have a Log out link

--- AlloyTraining/Controllers/PageControllerBase.cs
+++ AlloyTraining/Controllers/PageControllerBase.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
+using System.Web.Security;
 using AlloyTraining.Models.Pages;
 using EPiServer;
 using EPiServer.Core;
@@ -12,7 +13,18 @@ namespace AlloyTraining.Controllers
     public abstract class PageControllerBase<T> :
         PageController<T> where T : SitePageData
     {
-        
+        protected readonly IContentLoader loader;
+
+        public PageControllerBase(IContentLoader loader)
+        {
+            this.loader = loader;
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index");
+        }
         public ActionResult Index(SitePageData currentPage)
         {
             /* Implementation of action. You can create your own view model class that you pass to the view or
