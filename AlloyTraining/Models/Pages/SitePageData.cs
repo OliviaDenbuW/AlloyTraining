84b3256a44098de2de9b3ca2cac8bf5b0a4d1637
Olivia Considerit <olivia.denbu@gmail.com>
2019-01-09 10:08:05 +0100

    - Created the abstract class SitePageData.cs - Added four virtual properties

---
+++ AlloyTraining/Models/Pages/SitePageData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EPiServer.Core;
+
+namespace AlloyTraining.Models.Pages
+{
+    public abstract class SitePageData : PageData
+    {
+        public virtual string MetaTitle { get; set; }
+
+        public virtual string MetaKeywords { get; set; }
+
+        public virtual string MetaDescription { get; set; }
+
+        public virtual ContentReference PageImage { get; set; }
+
+        //public virtual string MetaTitle { get; set; }
+    }
+}
