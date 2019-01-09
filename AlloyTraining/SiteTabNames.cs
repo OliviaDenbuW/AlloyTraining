3319a6189b515555e05b9f6dff01ed4d4c902576
Olivia Considerit <olivia.denbu@gmail.com>
2019-01-09 10:21:45 +0100

    - Added the static class SiteTabNames.cs - Added two constants "SEO" and "SiteSettings" wich will be used as tabs - Added attribut for giving the tabs different access levels

---
+++ AlloyTraining/SiteTabNames.cs
@@ -0,0 +1,21 @@
+﻿using EPiServer.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Security;
+
+
+namespace AlloyTraining
+{
+    [GroupDefinitions]
+    public static class SiteTabNames
+    {
+        //The user must have "Edit access" to be able to see this tab = (Change access rights)
+        [Display(Order = 10)] //För att sortera horisontella tabbar
+        [RequiredAccess(AccessLevel.Edit)]
+        public const string SEO = "SEO";
+
+        //The user must have "Administer" access level to see this tab
+        [Display(Order = 20)]
+        [RequiredAccess(AccessLevel.Administer)]
+        public const string SiteSettings = "Site Settings";
+    }
+}
