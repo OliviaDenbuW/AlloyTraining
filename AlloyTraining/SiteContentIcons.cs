ab63d10928611f0c5ab0408d38f0205e3700ab43
Olivia Considerit <olivia.denbu@gmail.com>
2019-01-09 11:54:08 +0100

    - Added following six classes wich all inherited from ImageUrlAttribute.cs: 1. SitePageIconAttribute.cs 2. SiteBlockIconAttribute.cs 3. SiteStartIconAttribute.cs 4. SiteSearchIconAttribute.cs 5. SiteCommerceIconAttribute.cs 6. SiteContainerIconAttribute.cs

--- AlloyTraining/SiteContentIcons.cs
+++ AlloyTraining/SiteContentIcons.cs
@@ -18,4 +18,40 @@ namespace AlloyTraining
         public SiteImageUrlAttribute(string path)
             : base(path) { }
     }
+
+    public class SitePageIconAttribute : ImageUrlAttribute
+    {
+        public SitePageIconAttribute()
+            : base("~/Static/contenticons/epi-edu-icon-page.jpg") { }
+    }
+
+    public class SiteBlockIconAttribute : ImageUrlAttribute
+    {
+        public SiteBlockIconAttribute()
+        : base("~/Static/contenticons/epi-edu-icon-block.jpg") { }
+    }
+
+    public class SiteStartIconAttribute : ImageUrlAttribute
+    {
+        public SiteStartIconAttribute()
+        : base("~/Static/contenticons/epi-edu-icon-start.jpg") { }
+    }
+
+    public class SiteSearchIconAttribute : ImageUrlAttribute
+    {
+        public SiteSearchIconAttribute()
+        : base("~/Static/contenticons/epi-edu-icon-search.jpg") { }
+    }
+
+    public class SiteCommerceIconAttribute : ImageUrlAttribute
+    {
+        public SiteCommerceIconAttribute()
+        : base("~/Static/contenticons/epi-edu-icon-commerce.jpg") { }
+    }
+
+    public class SiteContainerIconAttribute : ImageUrlAttribute
+    {
+        public SiteContainerIconAttribute()
+        : base("~/Static/contenticons/epi-edu-icon-container.jpg") { }
+    }
 }
