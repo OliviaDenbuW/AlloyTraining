1c7ae91bc96ced196c33f7bbed29598359b318c4
Olivia Considerit <olivia.denbu@gmail.com>
2019-01-09 13:09:17 +0100

    - Added PageControllerBase<T>.cs - Made it abstract - Made it implement PageController<T> where T : SitePageData

---
+++ AlloyTraining/Controllers/PageControllerBase.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using AlloyTraining.Models.Pages;
+using EPiServer;
+using EPiServer.Core;
+using EPiServer.Framework.DataAnnotations;
+using EPiServer.Web.Mvc;
+
+namespace AlloyTraining.Controllers
+{
+    public abstract class PageControllerBase<T> :
+        PageController<T> where T : SitePageData
+    {
+        
+        public ActionResult Index(SitePageData currentPage)
+        {
+            /* Implementation of action. You can create your own view model class that you pass to the view or
+             * you can pass the page type for simpler templates */
+
+            return View(currentPage);
+        }
+    }
+}
