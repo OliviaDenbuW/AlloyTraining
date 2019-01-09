d8f4642a3d847741fb2a9fe3b51f4ee061eb3a09
Olivia Considerit <olivia.denbu@gmail.com>
2019-01-09 11:47:26 +0100

    - Created SiteImageUrlAttribute.cs - Added two constructors: 1. With url to the icon folder and 2. with a path parameter

--- AlloyTraining/SiteContentIcons.cs
+++ AlloyTraining/SiteContentIcons.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EPiServer.DataAnnotations;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +8,14 @@ namespace AlloyTraining
 {
     public class SiteContentIcons
     {
+    }
+
+    public class SiteImageUrlAttribute : ImageUrlAttribute
+    {
+        public SiteImageUrlAttribute()
+            : base("~/Static/contenticons/epi-edu-icon.jpg") { }
+
+        public SiteImageUrlAttribute(string path)
+            : base(path) { }
     }
 }
