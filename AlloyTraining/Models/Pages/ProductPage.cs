362cc839683dbfde6d002b08b2d49a8b6257c4c0
Olivia Considerit <olivia.denbu@gmail.com>
2019-01-10 15:46:48 +0100

    - Created ProductPage.cs - Made it inhetrit StandardPage.cs

---
+++ AlloyTraining/Models/Pages/ProductPage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+
+namespace AlloyTraining.Models.Pages
+{
+    [ContentType(DisplayName = "ProductPage", GUID = "aa2bd08b-e52e-486a-a135-532802296fc8", Description = "")]
+    public class ProductPage : StandardPage
+    {
+        /*
+                [CultureSpecific]
+                [Display(
+                    Name = "Main body",
+                    Description = "The main body will be shown in the main content area of the page, using the XHTML-editor you can insert for example text, images and tables.",
+                    GroupName = SystemTabNames.Content,
+                    Order = 1)]
+                public virtual XhtmlString MainBody { get; set; }
+         */
+    }
+}
