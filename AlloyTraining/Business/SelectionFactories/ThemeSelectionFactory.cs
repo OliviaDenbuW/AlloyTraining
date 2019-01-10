71cb36719ee431c927e16c63a485f9c004c7fb8a
Olivia Considerit <olivia.denbu@gmail.com>
2019-01-10 15:42:43 +0100

    - Added a SelectionFactory folder - Added ThemeSelectionFactory.cs to the folder

---
+++ AlloyTraining/Business/SelectionFactories/ThemeSelectionFactory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AlloyTraining.Business.SelectionFactories
+{
+    public class ThemeSelectionFactory
+    {
+    }
+}
