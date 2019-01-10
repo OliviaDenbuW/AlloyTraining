e61de4772fe60133749b819b4b9112429367a602
Olivia Considerit <olivia.denbu@gmail.com>
2019-01-10 15:54:26 +0100

    - Added SetDefaultValues() - Added two properties: Theme and UniqueSellingPoints with attributes - Added attributes to the class

--- AlloyTraining/Models/Pages/ProductPage.cs
+++ AlloyTraining/Models/Pages/ProductPage.cs
@@ -1,23 +1,41 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using AlloyTraining.Business.SelectionFactories;
 using EPiServer.Core;
 using EPiServer.DataAbstraction;
 using EPiServer.DataAnnotations;
+using EPiServer.Shell.ObjectEditing;
 using EPiServer.SpecializedProperties;
 
 namespace AlloyTraining.Models.Pages
 {
-    [ContentType(DisplayName = "ProductPage", GUID = "aa2bd08b-e52e-486a-a135-532802296fc8", Description = "")]
+    [ContentType(DisplayName = "Product",
+                 GroupName = SiteGroupNames.Specialized,
+                 Order = 20,
+                 GUID = "aa2bd08b-e52e-486a-a135-532802296fc8",
+                 Description = "Use this for software products that Alloy sells")]
+    [SiteCommerceIcon]
     public class ProductPage : StandardPage
     {
-        /*
-                [CultureSpecific]
-                [Display(
-                    Name = "Main body",
-                    Description = "The main body will be shown in the main content area of the page, using the XHTML-editor you can insert for example text, images and tables.",
-                    GroupName = SystemTabNames.Content,
-                    Order = 1)]
-                public virtual XhtmlString MainBody { get; set; }
-         */
+        [SelectOne(SelectionFactoryType = typeof(ThemeSelectionFactory))]
+        [Display(Name = "Color theme",
+                 GroupName = SystemTabNames.Content,
+                 Order = 310)]
+        public virtual string Theme { get; set; }
+
+        [CultureSpecific]
+        [Display(Name = "Unique selling points",
+            GroupName = SystemTabNames.Content,
+            Order = 320)]
+        [Required]
+        public virtual IList<string> UniqueSellingPoints { get; set; }
+
+        public override void SetDefaultValues(ContentType contentType)
+        {
+            base.SetDefaultValues(contentType);
+
+            Theme = "theme1";
+        }
     }
 }
