a64435f98dcd1bb9e8775c5c739723776d677004
Olivia Considerit <olivia.denbu@gmail.com>
2019-01-10 15:18:17 +0100

    - Created a StandardPage.cs as a standard page type

---
+++ AlloyTraining/Models/Pages/StandardPage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+
+namespace AlloyTraining.Models.Pages
+{
+    [ContentType(DisplayName = "Standard",
+                 GroupName = SiteGroupNames.Common,
+                 GUID = "2c906ad8-2058-4f92-85fa-055bc502d85e",
+                 Description = "Use this page type unless you need a more specialized one")]
+    [SitePageIcon]
+    public class StandardPage : SitePageData
+    {
+
+        [CultureSpecific]
+        [Display(
+            Name = "Main body",
+            Description = "The main body will be shown in the main content area of the page, using the XHTML-editor you can insert for example text, images and tables.",
+            GroupName = SystemTabNames.Content,
+            Order = 150)]
+        public virtual XhtmlString MainBody { get; set; }
+
+    }
+}
