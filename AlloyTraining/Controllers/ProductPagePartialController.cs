09a90d588c01004171cd29871f30ff2e93376b09
Olivia Considerit <olivia.denbu@gmail.com>
2019-01-11 08:50:00 +0100

    - Edited ProductPagePartialController.cs - Made Index() an override - Modified the return value to a partial view

--- AlloyTraining/Controllers/ProductPagePartialController.cs
+++ AlloyTraining/Controllers/ProductPagePartialController.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Web.Mvc;
 using AlloyTraining.Models.Pages;
+using AlloyTraining.Models.ViewModels;
 using EPiServer;
 using EPiServer.Core;
 using EPiServer.Framework.DataAnnotations;
@@ -9,18 +10,14 @@ using EPiServer.Web.Mvc;
 
 namespace AlloyTraining.Controllers
 {
-    public class ProductPagePartialController : PageControllerBase<ProductPage>
+    public class ProductPagePartialController : PartialContentController<ProductPage>
     {
-        public ProductPagePartialController(IContentLoader loader) : base(loader)
-        {
-
-        }
-        public ActionResult Index(ProductPage currentPage)
+        public override ActionResult Index(ProductPage currentPage)
         {
             /* Implementation of action. You can create your own view model class that you pass to the view or
              * you can pass the page type for simpler templates */
 
-            return View(CreatePageViewModel(currentPage));
+            return PartialView(PageViewModel.Create(currentPage));
         }
     }
 }
