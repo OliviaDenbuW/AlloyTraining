a2e283fb57d8b8c2d5a815c86c326dc0a1f0e005
Olivia Considerit <olivia.denbu@gmail.com>
2019-01-11 13:54:06 +0100

    - Created TeaserBlock.cs - Added four properties with suitable attributes: 1. TeaserHeading, 2. TeaserText, 3. TeaserImage and 4. TeaserLink

---
+++ AlloyTraining/Models/Blocks/TeaserBlock.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.Web;
+
+namespace AlloyTraining.Models.Blocks
+{
+    [ContentType(DisplayName = "Teaser",
+                 GUID = "26ce6606-7fe1-41a0-a6ea-4c68411d2034",
+                 Description = "Use this for rich text with heading, image and page link that will be reused in multiple places")]
+    [SiteBlockIcon]
+    public class TeaserBlock : BlockData
+    {
+        [CultureSpecific]
+        [Display(Name = "Heading", Order = 10)]
+        public virtual string TeaserHeading { get; set; }
+
+        [CultureSpecific]
+        [Display(Name = "Rich text", Order = 20)]
+        public virtual XhtmlString TeaserText { get; set; }
+
+        [Display(Name = "Image", Order = 30)]
+        [UIHint(UIHint.Image)]
+        public virtual ContentReference TeaserImage { get; set; }
+
+        [Display(Name = "Link", Order = 40)]
+        public virtual PageReference TeaserLink { get; set; }
+    }
+}       
