bcd484daf77e98066af3c8e3e573e2e80e670bf1
Olivia Considerit <olivia.denbu@gmail.com>
2019-01-11 13:56:25 +0100

    - Created TeaserBlockViewModel.cs - Added two "normal" properties: 1. CurrentBlock and 2. TodaysVisitorCOunt

---
+++ AlloyTraining/Models/ViewModels/TeaserBlockViewModel.cs
@@ -0,0 +1,15 @@
+﻿using AlloyTraining.Models.Blocks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AlloyTraining.Models.ViewModels
+{
+    public class TeaserBlockViewModel
+    {
+        public TeaserBlock CurrentBlock { get; set; }
+
+        public int TodaysVisitorCount { get; set; }
+    }
+}
