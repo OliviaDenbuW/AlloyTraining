f9ad2f2a74313ad52e0eb2771ecd2ef00e44338f
Olivia Considerit <olivia.denbu@gmail.com>
2019-01-11 10:02:31 +0100

    - Created AllPagesWidePartialController.cs and AllPagesNarrowPartialController.cs

--- AlloyTraining/Controllers/AllPagesPartialController.cs
+++ AlloyTraining/Controllers/AllPagesPartialController.cs
@@ -11,12 +11,31 @@ using EPiServer.Web.Mvc;
 namespace AlloyTraining.Controllers
 {
     [TemplateDescriptor(Inherited = true, Tags = new[] { SiteTags.Full }, AvailableWithoutTag = true)]
-    public class AllPagesAllPagesFullPartialController : PartialContentController<SitePageData>
+    public class AllPagesAllPagesFullPartialController 
+        : PartialContentController<SitePageData>
     {
         public override ActionResult Index(SitePageData currentPage)
         {
-            return PartialView(viewName: SiteTags.Full,
-            model: PageViewModel.Create(currentPage));
+            return PartialView(viewName: SiteTags.Full, model: PageViewModel.Create(currentPage));
         }
     }
+
+    [TemplateDescriptor(Inherited = true, Tags = new[] { SiteTags.Wide }, AvailableWithoutTag = false)]
+    public class AllPagesWidePartialController 
+        : PartialContentController<SitePageData>
+    {
+        public override ActionResult Index(SitePageData currentPage)
+        {
+            return PartialView(viewName: SiteTags.Wide, model: PageViewModel.Create(currentPage));
+        }
+    }
+    [TemplateDescriptor(Inherited = true, Tags = new[] { SiteTags.Narrow }, AvailableWithoutTag = false)]
+     public class AllPagesNarrowPartialController
+         : PartialContentController<SitePageData>
+    {
+        public override ActionResult Index(SitePageData currentPage)
+        {
+            return PartialView(viewName: SiteTags.Narrow, model: PageViewModel.Create(currentPage));
+        }
+    }
 }
