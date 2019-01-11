2b652d163c00b4ac82cfb772135922a229eac6d0
Olivia Considerit <olivia.denbu@gmail.com>
2019-01-11 10:50:37 +0100

    - Added two properties to ProductPage.cs: 1. RelatedContentArea and 2.MainContentArea with appropriate attributes

--- AlloyTraining/Models/Pages/ProductPage.cs
+++ AlloyTraining/Models/Pages/ProductPage.cs
@@ -31,6 +31,18 @@ namespace AlloyTraining.Models.Pages
         [Required]
         public virtual IList<string> UniqueSellingPoints { get; set; }
 
+        [Display(Name = "Main content area",
+                 Description = "Drag and drop blocks and pages with partial templates.",
+                 GroupName = SystemTabNames.Content,
+                 Order = 330)]
+        public virtual ContentArea MainContentArea { get; set; }
+
+        [Display(Name = "Related content area",
+                 Description = "Drag and drop blocks and pages with partial templates.",
+                 GroupName = SystemTabNames.Content,
+                 Order = 340)]
+        public virtual ContentArea RelatedContentArea { get; set; }
+
         public override void SetDefaultValues(ContentType contentType)
         {
             base.SetDefaultValues(contentType);
