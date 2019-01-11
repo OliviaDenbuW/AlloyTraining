fa76113efdfd735f114737e4b4b0ba8befeb4477
Olivia Considerit <olivia.denbu@gmail.com>
2019-01-11 09:56:05 +0100

    - Created AllPagesPartialController.cs - Added AllPagesFullPartialController.cs

---
+++ AlloyTraining/Controllers/AllPagesPartialController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using AlloyTraining.Models.Pages;
+using AlloyTraining.Models.ViewModels;
+using EPiServer;
+using EPiServer.Core;
+using EPiServer.Framework.DataAnnotations;
+using EPiServer.Web.Mvc;
+
+namespace AlloyTraining.Controllers
+{
+    [TemplateDescriptor(Inherited = true, Tags = new[] { SiteTags.Full }, AvailableWithoutTag = true)]
+    public class AllPagesAllPagesFullPartialController : PartialContentController<SitePageData>
+    {
+        public override ActionResult Index(SitePageData currentPage)
+        {
+            return PartialView(viewName: SiteTags.Full,
+            model: PageViewModel.Create(currentPage));
+        }
+    }
+}
