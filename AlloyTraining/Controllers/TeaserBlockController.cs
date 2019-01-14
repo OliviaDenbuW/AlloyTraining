739107e51da3dd34a2471328ee90990fa6523efa
Olivia Considerit <olivia.denbu@gmail.com>
2019-01-14 09:54:17 +0100

    - Added the attribute [TemplateDescriptor] to TeaserBlockCOntroller.cs - Created TeaserBlockWideController.cs

--- AlloyTraining/Controllers/TeaserBlockController.cs
+++ AlloyTraining/Controllers/TeaserBlockController.cs
@@ -7,11 +7,13 @@ using AlloyTraining.Models.Blocks;
 using AlloyTraining.Models.ViewModels;
 using EPiServer;
 using EPiServer.Core;
+using EPiServer.Framework.DataAnnotations;
 using EPiServer.Web;
 using EPiServer.Web.Mvc;
 
 namespace AlloyTraining.Controllers
 {
+    [TemplateDescriptor(Tags = new[] { SiteTags.Full}, AvailableWithoutTag = true)]
     public class TeaserBlockController : BlockController<TeaserBlock>
     {
         public override ActionResult Index(TeaserBlock currentBlock)
@@ -23,6 +25,21 @@ namespace AlloyTraining.Controllers
             };
 
             return PartialView(viewmodel);
+        }
+    }
+
+    [TemplateDescriptor(Tags = new[] { SiteTags.Full })]
+    public class TeaserBlockWideController : BlockController<TeaserBlock>
+    {
+        public override ActionResult Index(TeaserBlock currentBlock)
+        {
+            var viewModel = new TeaserBlockViewModel
+            {
+                CurrentBlock = currentBlock,
+                TodaysVisitorCount = (new Random()).Next(300, 900)
+            };
+
+            return PartialView(viewModel);
         }
     }
 }
