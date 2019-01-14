186fdd7ecf7bfc303366646ba9ebbea852f67e0b
Olivia Considerit <olivia.denbu@gmail.com>
2019-01-14 11:42:08 +0100

    - Created ListingBlockController.cs for creating page listening blocks (already created a block and a viewModel)

---
+++ AlloyTraining/Controllers/ListingBlockController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AlloyTraining.Models.Blocks;
+using EPiServer;
+using EPiServer.Core;
+using EPiServer.Web;
+using EPiServer.Web.Mvc;
+
+namespace AlloyTraining.Controllers
+{
+    public class ListingBlockController : BlockController<ListingBlock>
+    {
+        public override ActionResult Index(ListingBlock currentBlock)
+        {
+            return PartialView(currentBlock);
+        }
+    }
+}
